38ed42d1ac7fc723f463ca851a23dce595509028
jorgeAgoiz <jorgeagoiz@gmail.com>
2020-12-28 21:50:07 +0100

    Added npms and configurated

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
+//Set enviroment variables
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
-
-const express = require("express");
-const app = express();
 const PORT = process.env.PORT;
 
+//Packages
+const path = require('path');
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+
+//Middlewares
+app.use(express.static(path.join(__dirname, 'public')));//To set the static files (css, js, etc)
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Set the template engine EJS
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+//Route to test
+app.get('/', ( req, res, next ) => {
+  res.render('main.ejs', {
+    pageTitle: "Probando Probando"
+  })
+});
+
+
+
+//Listen in .env.PORT
 app.listen(PORT, () => {
   console.log(`Listening in port: ${PORT}`);
 });
