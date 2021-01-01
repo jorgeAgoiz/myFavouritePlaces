bcfc920f6e2e4c2ba2faac1e8f6167d1b034d407
jorgeAP <jorgeagoiz@gmail.com>
2021-01-01 17:12:39 +0100

    Added errors message in views

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,6 +11,7 @@ exports.main = (req, res, next) => {
 exports.getSignIn = (req, res, next) => {
   res.render("signin.ejs", {
     pageTitle: "Sign In",
+    errorMessage: null,
   });
 };
 
@@ -19,16 +20,29 @@ exports.postSignIn = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     console.log(errors);
-    return res.redirect("/");
+    return res.status(422).render("signin.ejs", {
+      pageTitle: "Sign In",
+      errorMessage: errors.array()[0].msg,
+    });
   }
 
   return User.findOne({ email: email })
     .then(async (result) => {
+      if (!result) {
+        return res.status(422).render("signin.ejs", {
+          pageTitle: "Sign In",
+          errorMessage: "Email not found",
+        });
+      }
       const doMatch = await bcrypt.compare(password, result.password);
       if (doMatch) {
         return res.redirect("/usermenu");
       }
-      return res.redirect("/");
+
+      return res.status(422).render("signin.ejs", {
+        pageTitle: "Sign In",
+        errorMessage: "Incorrect Password",
+      });
     })
     .catch((err) => console.log(err));
 };
@@ -45,7 +59,6 @@ exports.postSignUp = async (req, res, next) => {
   const errors = validationResult(req);
   const emailBussy = await User.findOne({ email: email });
   if (emailBussy) {
-    console.log("Email is already registered.");
     return res.status(422).render("signup.ejs", {
       pageTitle: "Sign Up",
       errorMessage: "Email is already registered.",
